1d5f75797d72193e224c586cebc0cdc999263789
CLVal <valecl@comunidad.unam.mx>
2022-05-26 15:42:48 -0500

    Js mejorado

--- dynamics/index.js
+++ dynamics/index.js
@@ -5,10 +5,10 @@ ctx.fillStyle ="#000000";
 ctx.fillRect(0,0, canvas.width, canvas.height);
 ctx.fillStyle ="#00ff00";
 
-let x=60;
-let y=60;
-let mov=60;
-ctx.fillRect(x,y, 60,60);
+let x=45;
+let y=45;
+let mov=45;
+ctx.fillRect(x,y, 45,45);
 
 document.addEventListener('keydown', (event) => {
     var tecla = event.key;
@@ -18,28 +18,33 @@ document.addEventListener('keydown', (event) => {
     switch (tecla){
         case 'ArrowUp': 
             console.log(tecla);
-            y-=mov;
+            if(y>0)         //no deja que se salga del canvas
+                y-=mov;
             break;
 
         case 'ArrowDown':
             console.log(tecla);
-            y+=mov;
+            if(y<585)       //no deja que se salga del canvas
+                y+=mov;
             break;
 
         case 'ArrowRight':
             console.log(tecla);
-            x+=mov;
+            if(x<675)      //no deja que se salga del canvas
+                x+=mov;
+            console.log(x);
             break;
 
         case 'ArrowLeft':
             console.log(tecla);
-            x-=mov;
+            if(x>0)         //no deja que se salga del canvas
+                x-=mov;
             break;
 
         default:
             console.log("Esa tecla no vale")
     }
     ctx.fillStyle ="#00ff00";
-    ctx.fillRect(x,y, 60,60);
+    ctx.fillRect(x,y, 45,45);
 
 });
